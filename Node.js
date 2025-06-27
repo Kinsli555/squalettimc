8723b394effc89557893b6742eb28f717b1dbf59
Kinsli555 <Kislicardamone@gmail.com>
2025-06-27 23:41:32 +0200

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,12 +1,1 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = 3000;
 
-// Servire i file statici dalla cartella "public"
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Avvia il server
-app.listen(port, () => {
-  console.log(`Sito web avviato su http://squalettimc:${port}`);
-});
